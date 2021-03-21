5419f2b9aa6c73110fb59fc96c28225f49c97c0f
Jon Sweeney <jsweeney@quantummetric.com>
2021-03-21 15:05:41 -0700

    complete MemeMe's requirements

--- MemeMe/Controller/MemeEditorExtensions.swift
+++ MemeMe/Controller/MemeEditorExtensions.swift
@@ -16,17 +16,36 @@ extension MemeEditorViewController: UITextFieldDelegate {
         tf.defaultTextAttributes = memeTextAttributes
         tf.textAlignment = .center
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
 // MARK: - UI Image Picker Controller Delegate
 
 extension MemeEditorViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    func getImageFromCameraOrPhoto(source: UIImagePickerController.SourceType) {
+    func getImageFrom(source: UIImagePickerController.SourceType) {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
         pickerController.allowsEditing = true
         pickerController.sourceType = source
         
         present(pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true)
+        guard let image = info[.originalImage] as? UIImage else {
+            self.showAlert(K.AlertMsg.ImportPhotoErrorTitle, message: K.AlertMsg.ImportPhoroErrorMsg)
+            return
+        }
+        imageIV.image = image
+        shareBtn.isEnabled = true
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
--- MemeMe/Controller/MemeEditorViewController.swift
+++ MemeMe/Controller/MemeEditorViewController.swift
@@ -11,6 +11,7 @@ class MemeEditorViewController: UIViewController {
     
     // MARK: Outlets
     
+    @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var shareBtn: UIBarButtonItem!
     @IBOutlet weak var cancelBtn: UIBarButtonItem!
     @IBOutlet weak var cameraBtn: UIBarButtonItem!
@@ -18,13 +19,14 @@ class MemeEditorViewController: UIViewController {
     @IBOutlet weak var topTF: UITextField!
     @IBOutlet weak var bottomTF: UITextField!
     @IBOutlet weak var imageIV: UIImageView!
+    @IBOutlet weak var toolbar: UIToolbar!
     
     // MARK: Variables
     
     let topText = K.textTopDefault
     let bottomText = K.textBottomDefault
     
-    var memeImage: UIImage! = nil
+    var memedImage: UIImage! = nil
     var meme: Meme? = nil
     
     // MARK: - Meme's Text Attributes
@@ -48,7 +50,7 @@ class MemeEditorViewController: UIViewController {
         configureTextField(textField: topTF, text: K.textTopDefault)
         configureTextField(textField: bottomTF, text: K.textBottomDefault)
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
@@ -62,51 +64,68 @@ class MemeEditorViewController: UIViewController {
         // 0 - camera  1 - album
         if sender.tag == 0 {
             // Take photo
-            print("Taking photo...")
+            getImageFrom(source: .camera)
         } else {
-            // Get Album Photo
-            print("Picking photo from album...")
+            // Get Photo Library
+            getImageFrom(source: .photoLibrary)
         }
     }
     
     // Share the meme
     @IBAction func shareMeme(_ sender: Any) {
-        // share meme
-        print("Sharing meme...")
+        memedImage = generateMemedImage()
+        let aVC = UIActivityViewController(activityItems: [memedImage!], applicationActivities: nil)
+        aVC.popoverPresentationController?.sourceView = self.view
+        self.present(aVC, animated: true, completion: nil)
+        aVC.completionWithItemsHandler = {(activity, success, items, error) in
+            if success {
+                self.meme = self.saveMeme()
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
     }
     
     
     // Reset the meme
     @IBAction func resetMeme(_ sender: Any) {
-        // reset meme
-        print("Reseting meme...")
+        memedImage = nil
+        resetUI()
+        dismiss(animated: true, completion: nil)
     }
     
     // MARK: - Functionality
     
     func saveMeme() -> Meme? {
-        let meme = Meme(textTop: "", textBottom: "", imageOriginal: UIImage(), imageEdited: UIImage())
-        // Do stuff
+        let image = imageIV.image ?? UIImage(named: "default")
+        let meme = Meme(textTop: topTF.text!, textBottom: bottomTF.text!, imageOriginal: image!, imageEdited: memedImage)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.self.reloadInputViews()
         
         return meme
     }
     
     func generateMemedImage() -> UIImage {
-
-        // TODO: Hide toolbar and navbar
-
+        // Hide Nav and Toolbar
+        hideBars(true)
+        
         // Render view to an image
         UIGraphicsBeginImageContext(self.view.frame.size)
         view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
-        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
-
-        // TODO: Show toolbar and navbar
-
+        
+        // Show Nav and Toolbar
+        hideBars(false)
+        
         return memedImage
     }
     
     // MARK: - Helpers
+    
+    func hideBars(_ hide: Bool) {
+        navBar.isHidden = hide
+        toolbar.isHidden = hide
+    }
     
     func getKeyboardHeight(_ notification: Notification) -> CGFloat {
         let userInfo = notification.userInfo
@@ -126,7 +145,7 @@ class MemeEditorViewController: UIViewController {
             view.frame.origin.y = 0
         }
     }
-
+    
     func subscribeToKeyboardNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -141,6 +160,13 @@ class MemeEditorViewController: UIViewController {
         configureTextField(textField: bottomTF, text: K.textBottomDefault)
         imageIV.image = nil
         shareBtn.isEnabled = false
+    }
+    
+    // Show alert to user
+    func showAlert(_ title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: K.AlertMsg.DismissAlert, style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
